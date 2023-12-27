791b42afd2be852368cebabd2f778462ad954c64
Sergio Rubio <HG86revient@hotmail.com>
2023-12-27 16:45:51 -0600

    EF core NugetPkg added to persist changes in sql server, PlaylistController added. Playlist Entity created. TODO: check if CoverImage field should be a filepath or a Byte[]

---
+++ Controllers/PlaylistController.cs
@@ -0,0 +1,22 @@
+﻿using ElectricSheep.Model.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricSheep.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        //here we must inject our service
+        //also a logger is needed
+        [Route("[controller]/MyPlaylists")]
+        [HttpPost]
+        public Playlist NewPlaylist([FromBody] Guid userID)
+        {
+            return new Playlist("DemoPlaylist", "some descriptive Text", userID);
+        }
+        [HttpPost]
+        public List<Playlist> GetPlaylists([FromBody] Guid userID)
+        {
+            return new List<Playlist>();
+        }
+    }
+}
---
+++ Model/Entity/Playlist.cs
@@ -0,0 +1,29 @@
+﻿namespace ElectricSheep.Model.Entity
+{
+    public class Playlist
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public Guid creator { get; set; }
+        public int songCount { get; set; }
+        public string totalDuration { get; set; }
+        public List<string> Songs { get; set; }
+        public Byte[] CoverImage { get; set; }
+        public DateTime createdDate { get; set; }
+        public DateTime updatedDate { get; set; }
+        public Playlist(string nombre, string descripcion, Guid creador) 
+        {
+            id = Guid.NewGuid();
+            name = nombre;
+            description = descripcion;
+            creator = creador;
+            songCount = 0;
+            totalDuration = "00:00";
+            Songs = new List<string>();
+            CoverImage = new Byte[0];
+            createdDate = DateTime.Now;
+            updatedDate = DateTime.Now;
+        }
+    }
+}
